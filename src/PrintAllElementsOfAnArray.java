994440e2e26df3b2c3786ffd9c6f17c83e2217f9
shravanikummari <shravanikummari1234@gmail.com>
2024-06-30 09:01:38 -0700

    Added PrintAllElementsOfAnArray class successfully

---
+++ src/PrintAllElementsOfAnArray.java
@@ -0,0 +1,9 @@
+public class PrintAllElementsOfAnArray {
+    public static void main(String[] args){
+        int[] array={8,4,3,2,1,7};
+        System.out.println("Elements of the array:");
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
