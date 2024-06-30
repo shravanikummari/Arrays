5c20a61cad225cdef3ddeab69c1901658daa11f1
shravanikummari <shravanikummari1234@gmail.com>
2024-06-30 08:57:24 -0700

    Added sum of arrays class successfully

---
+++ src/Arrays.java
@@ -0,0 +1,13 @@
+
+public class Arrays {
+    public static void main(String[] args){
+                int [] arr = new int [] {13, 14, 15, 16, 17};
+                int sum = 0;
+                for (int i = 0; i < arr.length; i++) {
+                    sum = sum + arr[i];
+                }
+                System.out.println("Sum of all the elements of an array: " + sum);
+            }
+        }
+
+
